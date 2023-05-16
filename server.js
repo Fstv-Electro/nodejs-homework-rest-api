92109f718820648f15708b4a66d6f95d3998a4fa
Oleksandr Kutsenko <104622826+Fstv-Electro@users.noreply.github.com>
2023-05-16 17:15:32 +0300

    added mongoose

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
-const app = require('./app')
+const app = require("./app");
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST =
+  "mongodb+srv://fstv_electro:Crdi6iVOFpYt0JTA@cluster0.thdexvi.mongodb.net/my_contacts?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
